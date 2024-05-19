59b0a3fbd2306a0aac1cbc9975c391793ecffa1a
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-05-19 16:57:01 +0300

    "Deleted file A_Boy_or_Girl.py"

--- A_Boy_or_Girl.py
+++
@@ -1,2 +0,0 @@
-s = set(str(input()))
-print("CHAT WITH HER!" if len(s) % 2 == 0 else "IGNORE HIM!")
---
+++ A_Sequencer.py
@@ -0,0 +1,15 @@
+t = int(input().strip())
+
+for _ in range(t):
+    x = int(input())
+    a = list(map(int, input().split()))
+    l, r = 0, len(a) - 1
+    ans = []
+    while l < r:
+        ans.append(a[l])
+        ans.append(a[r])
+        l += 1
+        r -= 1
+    if l == r:
+        ans.append(a[l])
+    print(*ans)
